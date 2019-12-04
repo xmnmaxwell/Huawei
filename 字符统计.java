2cd08265732792e61ac75a19326ed4f8a45e7b42
mnxu819 <mnxu819@tamu.edu>
2019-12-04 16:05:16 -0600

    Create 字符统计.java

---
+++ 字符统计.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+   
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String line=null;
+        while((line=br.readLine())!=null){
+            System.out.println(fun(line));
+        }
+    }
+   
+    private static String fun(String str) {
+        char[] chs=str.toCharArray();
+        int[] num=new int[200];     //必须大于128
+        for (char c : chs) {
+            int i=(int)c;
+            num[i]++;
+        }
+           
+        int max=0;
+        for(int i=0;i<num.length;i++){
+            if(max<num[i]){
+                max=num[i];
+            }
+        }
+           
+        StringBuffer sb=new StringBuffer();
+           
+        while(max!=0){
+            for(int i=0;i<num.length;i++){
+                if(max==num[i]){
+                    sb.append((char)i);
+                }
+            }
+            max--;
+        }
+        return sb.toString();
+    }
+}
