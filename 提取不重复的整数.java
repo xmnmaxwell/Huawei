6fb4a4e27425de097f00408e6b9b553039b9c1a6
mnxu819 <mnxu819@tamu.edu>
2019-12-04 18:15:40 -0600

    Update 提取不重复的整数.java

--- 提取不重复的整数.java
+++ 提取不重复的整数.java
@@ -1,18 +1,21 @@
+输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
 import java.util.Scanner;
+ 
 public class Main {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner scanner=new Scanner(System.in);
-        while(scanner.hasNext()){
-            String s=scanner.nextLine();
-            int len=s.length();
-            int []arr1=new int[10];
-            for(int i=len-1;i>=0;i--){
-                if(arr1[s.charAt(i)-48]==0){
-                    System.out.print(s.charAt(i)-48);
-                    arr1[s.charAt(i)-48]++;
-                }
+        Scanner str = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        sb.append(str.next());
+        String st = sb.reverse().toString();
+        char[] ch = st.toCharArray();
+        String jud = "";
+        for (int i = 0; i < ch.length ; i++){
+            String s1 = String.valueOf(ch[i]);
+//          System.out.println(s1);
+            if(!jud.contains(s1)){
+                jud = jud + s1;
             }
         }
+        System.out.println(Integer.parseInt(jud));
     }
 }
